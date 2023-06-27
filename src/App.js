2f0655260edb623938cf7d482e53e5eb378def57
Rita HC <ritahameerchauhan@gmail.com>
2023-06-27 19:28:07 -0400

    LinkedInPoster ready

---
+++ src/App.js
@@ -0,0 +1,63 @@
+
+
+
+import React from "react";
+import './index.css';
+
+const BackgroundImagePage = () => {
+  return (
+    <div className="bg ">
+      <div className="Icons text-center mt-1" style={{ margin: "-10px" }}>
+        <img
+          style={{ height: "100px", width: "100px", padding: "10px" }}
+          src="https://cdn-icons-png.flaticon.com/128/1199/1199124.png"
+          alt="JavaScript"
+        />
+      
+        <img
+          style={{ height: "100px", width: "100px", padding: "10px" }}
+          src="https://cdn-icons-png.flaticon.com/128/919/919838.png"
+          alt="Golang"
+        />
+        <img
+          style={{ height: "100px", width: "100px", padding: "10px" }}
+          src="https://cdn-icons-png.flaticon.com/128/5968/5968350.png"
+          alt="Python"
+        />
+        <img
+          style={{ height: "100px", width: "100px", padding: "10px" }}
+          src="https://cdn-icons-png.flaticon.com/128/2232/2232241.png"
+          alt="Databases"
+        />
+       
+          <img
+            style={{ height: "100px", width: "100px", padding: "10px" }}
+            src="https://cdn-icons-png.flaticon.com/128/1183/1183672.png"
+            alt="React"
+          />
+       
+        <img
+          style={{ height: "100px", width: "100px", padding: "5px" }}
+          src="https://cdn-icons-png.flaticon.com/128/5968/5968875.png"
+          alt="Jira"
+        />
+
+        <img
+          style={{ height: "100px", width: "100px", padding: "10px" }}
+          src="https://cdn-icons-png.flaticon.com/128/2111/2111425.png"
+          alt="GitHub"
+        />
+      </div>
+      <div className=" Selfie text-center" style={{ margin: "-100" }}>
+        <img
+          src="./Transparent.png"
+          className="img-fluid"
+          alt=""
+          style={{ height: "450px", width: "300px" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default BackgroundImagePage;
